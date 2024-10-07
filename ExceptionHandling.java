49ce1f9ff07c0ad3b2b117f3ece0e3017cd320ac
shubhamni08 <nikamshubham016@gmail.com>
2024-10-07 13:39:09 +0530

    Exception Handling code Added

---
+++ ExceptionHandling.java
@@ -0,0 +1,20 @@
+package leetcode_String;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ExceptionHandling {
+
+	public static void main(String[] args)  {
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter("abc.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(fw.toString());
+	}
+
+}
