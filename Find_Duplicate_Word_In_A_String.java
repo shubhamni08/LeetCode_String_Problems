51e78f9a5f182878da769802e8e86208bd13c240
shubhamni08 <nikamshubham016@gmail.com>
2024-10-08 09:21:20 +0530

    Duplicate word in a string code added

---
+++ Find_Duplicate_Word_In_A_String.java
@@ -0,0 +1,11 @@
+package leetcode_String;
+
+public class Find_Duplicate_Word_In_A_String {
+
+	public static void main(String[] args) {
+		String s = "Bread butter and bread";
+//		output = bread
+
+	}
+
+}
