c07320ed9e07b485f60be723e69f4976ae7fb5f5
shubhamni08 <nikamshubham016@gmail.com>
2024-10-19 17:41:09 +0530

    Split string code added

---
+++ Split_String.java
@@ -0,0 +1,24 @@
+package leetcode_String;
+
+public class Split_String {
+
+	public static void main(String[] args) {
+		String s = "Java,Python,SQL,TestNG,   Maven   ";
+
+		split(s);
+
+	}
+
+	private static void split(String s) {
+		
+		String[] s1 = s.split(",");
+		
+		for(String x:s1) {
+//			x=x.replaceAll("\\s", "");
+			x=x.trim();
+			System.out.print(x+" ");
+		}
+		
+	}
+
+}
